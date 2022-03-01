b24c0bf7368cbd826e744e617669368747d6d08f
diopp <49011476+diopp@users.noreply.github.com>
2022-03-01 06:22:36 -0800

    set-up

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_app',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSMOT97',
+        'HOST': 'localhost',
     }
 }
 
